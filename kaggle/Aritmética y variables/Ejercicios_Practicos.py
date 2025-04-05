60c6474c147147d0a655272cb7afa092fcb8af60
FrenKsxr <frenyersegura13@outlook.com>
2025-04-04 20:58:16 -0600

    Ejercicios arimeticos y variables

--- kaggle/Aritmética y variables/Ejercicios_Practicos.py
+++ kaggle/Aritmética y variables/Ejercicios_Practicos.py
@@ -21,3 +21,9 @@ print("El área del rectángulo es:", base * altura, "m²")
 grados_celsius = 20
 grados_fahrenheit = (grados_celsius * 9/5) + 32
 print("Grados Celsius:", grados_celsius, "Grados Fahrenheit:", grados_fahrenheit)
+
+# Ejercicio 4: calcular el área de un círculo
+radio = 5
+pi = 3.14159
+area_circulo = pi * (radio ** 2)
+print("El área del círculo es:", area_circulo, "m²")
